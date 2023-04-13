b499145602d9afa42a4475f17d62cf1cae6e17ae
Mas Gading <anakmasgadinggondokusumo@gmail.com>
2023-04-13 13:55:33 +0700

    reduced junks

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from os import path, makedirs
 from sys import exit
-from tempfile import gettempdir
 
 import subprocess
-import requests
 import json
 import re
-import traceback
 
 # text styles
 red = '\033[0;31m'
@@ -18,12 +15,10 @@ reset = '\033[0m'
 # vars
 update_link = 'https://autopatchhk.yuanshen.com/' # update server
 genshin_data = '/sdcard/Android/Data/com.miHoYo.GenshinImpact/' # game files
-temp = gettempdir()
 
 working_dir = path.dirname(__file__)
 
 def log(str):
-    #print(str)
     print('{color}[i]: {reset}{str}{reset}'.format(color = cyan, reset = reset, str = str))
 
 def error(str):
@@ -31,18 +26,6 @@ def error(str):
 
 def new_input(str):
     return input('{color}[?]: {reset}{str}{reset}'.format(color = green, reset = reset, str = str))
-
-def download_file(url, filename):
-    try:
-        r = requests.get(url)
-        with open(filename, 'wb') as file:
-            file.write(r.content)
-            file.close()
-    except Exception as e:
-        traceback.print_exc()
-        traceback.print_stack()
-        error(e)
-        exit(1)
 
 def pull_files():
     log('waiting for android device...')
@@ -60,6 +43,8 @@ def pull_files():
     subprocess.run(['adb', 'pull', genshin_data + 'files/base_revision'], stdout=subprocess.DEVNULL)
     log('fetching audio_lang_14...')
     subprocess.run(['adb', 'pull', genshin_data + 'files/audio_lang_14'], stdout=subprocess.DEVNULL)
+    log('fetching data_versions_remote...')
+    subprocess.run(['adb', 'pull', genshin_data + 'files/data_versions_remote'], stdout=subprocess.DEVNULL)
     log('fetching res_versions_remote...')
     subprocess.run(['adb', 'pull', genshin_data + 'files/res_versions_remote'], stdout=subprocess.DEVNULL)
     log('fetching ScriptVersion...')
@@ -80,15 +65,15 @@ if __name__ == '__main__':
     #######
 
     print('')
-    if path.exists(working_dir + '/base_revision') and path.exists(working_dir + '/audio_lang_14') and path.exists('res_versions_remote') and path.exists(working_dir + '/ScriptVersion'):
-        log('Files used to check game version are found.')
-        response = new_input('Do you want to use them instead? [y/n]: ')
+    if path.exists(working_dir + '/base_revision') and path.exists(working_dir + '/audio_lang_14') and path.exists('data_versions_remote') and path.exists('res_versions_remote') and path.exists(working_dir + '/ScriptVersion'):
+        log('files used to check game version are found.')
+        response = new_input('do you want to use them instead? [y/n]: ')
         if response.lower() == 'n': 
             pull_files()
         elif response.lower() == 'y':
             pass
         else:
-            error('Please input a valid response.')
+            error('please input a valid response.')
             exit(1)
     else:
         pull_files()
@@ -116,7 +101,7 @@ if __name__ == '__main__':
     )
 
     # convert res_versions_remote to readable dictionary
-    log('parsing res_versions_remote...')
+    log('parsing res_versions_remote and data_versions_remote...')
     resources = []
     audio_resources = {
         'English(US)': [],
@@ -125,10 +110,11 @@ if __name__ == '__main__':
         'Chinese': []
     }
 
-    required_files = open(working_dir + '/res_versions_remote', 'r').read().split('\n')
-    required_files.pop() # remove last element
+    required_files_1 = open(working_dir + '/res_versions_remote', 'r').read().split('\n')
+    # required_files_2 = open(working_dir + '/data_versions_remote', 'r').read().split('\n')
 
-    for file in required_files:
+    for file in required_files_1:
+        if file == '': continue
         json_file = json.loads(file)
         if re.search('^English\\(US\\)/.+\\.pck', json_file['remoteName']):
             audio_resources['English(US)'].append(json_file)
@@ -141,7 +127,11 @@ if __name__ == '__main__':
         else:
             resources.append(json_file)
 
-    #print(resources)
+    # for file in required_files_2:
+    #     if file == '': continue
+    #     json_file = json.loads(file)
+    #     resources.append(json_file)
+
     # start download
     if not path.isdir('files'):
         log('creating data directory...')
@@ -156,6 +146,7 @@ if __name__ == '__main__':
         # TODO: do a checksum check from the information we've gathered
  
     print('')
+    log('performing resource download...')
     for file in resources:
         suffix = ''
         if re.search('\\.blk$', file['remoteName']):
@@ -165,41 +156,32 @@ if __name__ == '__main__':
         elif re.search('\\.pck$', file['remoteName']):
             suffix = 'AudioAssets/' 
 
-        filepath = 'files/' + suffix + file['remoteName']
+        filepath = working_dir + '/files/' + suffix + file['remoteName']
         if path.isfile(filepath) == False:
-            #log('downloading {file}...'.format(file = suffix + file['remoteName']))
-            #log('url ' + endpoint + suffix + file['remoteName'])
             dir = path.dirname(file['remoteName'])
             if dir != '':
                 makedirs('files/' + suffix + dir, 0o777, True)
-
-            # download_file(endpoint + suffix + file['remoteName'], 'files/' + suffix + file['remoteName'])
-            # use aria2c
-
+            
             subprocess.run(['aria2c', 
                 '--out=' + filepath,
                 '--max-concurrent-downloads=8', '--max-connection-per-server=8',
                 '--download-result=hide', '--continue=true', endpoint + suffix + file['remoteName']
             ])
-        else:
-            log(filepath + ' already existed, skipping...')
-                
-    for pck in audio_resources[genshin_audio_language]:
-        # log('downloading {file}'.format(file = 'AudioAssets/' + pck['remoteName']))
-        # download_file(endpoint + 'Audio/' + pck['remoteName'], 'files/' + 'Audio/' + pck['remoteName'])
-        # use aria2c
 
-        filepath = 'files/AudioAssets/'+ pck['remoteName']
+    log('downloading ' + genshin_audio_language + ' voice pack...') 
+    for pck in audio_resources[genshin_audio_language]:
+        filepath = working_dir + '/files/AudioAssets/'+ pck['remoteName']
 
         if path.isfile(filepath) == False:
             subprocess.run(['aria2c',
                 '--out=' + filepath,
                 '--max-concurrent-downloads=8', '--max-connection-per-server=8',
-                '--download-result=hide', '--continue=true', endpoint + 'AudioAssets/' + file['remoteName']
+                '--downloads-result=hide', '--continue=true', endpoint + 'AudioAssets/' + pck['remoteName']
             ])
-        else:
-            log(filepath + ' already existed, skipping...')
     log('done!')
+
+    log('some files are not downloaded due to the game server not storing the files, but this is enough to get the game up and running!')
 else:
     print('Please do not import this file, run it with python instead.')
     print('python main.py')
+    exit()
